ea05af2c331b9553cc72f5808904c7a0f4fa2eef
Gerrit Roellinghoff <gerritr@lfs2.mpi-hd.mpg.de>
2024-07-09 09:52:35 +0200

    Improved tutorials, added tycho supplementary data to gaiadr3, removed pandas dependency and worked on readme

--- README.md
+++ README.md
@@ -8,16 +8,18 @@
 
 # nsb2: Simulating Night Sky Background for IACTs
 
-This is a refactored and reworked version of Matthias Buecheles [nsb](https://pypi.org/project/nsb/) package. It includes the following improvements:
+nsb2 is a python library for calculating the effect of night sky background (NSB) on telescopes with large pixel areas, namely imaging air cherenkov telescopes (IACTs). It includes models from theory / experimental data for star light, zodiacal light, moon light, diffuse galactic light and airglow. It also models atmospheric effects, such as rayleigh scattering, mie scattering, single scattering albedo and aerosol concentration. 
 
-- Per Pixel NSB rates
-- Modularity + Support for other IACTs
-- More physical
-- Significantly faster
+nsb2 offers an interface with ctapipe, enabling adhoc simulation of simple telescope models from ctapipe geometries as well as making use of ctapipe plotting capability. Depending on the complexity of the starfield simulated, nsb2 can simulate NSB on the order of ~1sec per field.
 
 # Installation
 This package can be installed via pip via:
 
 pip install git+https://git.ecap.work/groellinghoff/nsb2.git
 
+nsb2 requires blacksky, histlite, numpy, scikit-learn, astropy and healpy.
+
 Some of the supplementary data (fits file and gaia catalogs) are large and not on this git repo. They can either be created/downloaded manually (see the blacksky examples on how to create a gaia catalog/map). Alternatively, you can get the larger files directly from me.
+
+## Optional dependencies
+For plotting, nsb2 relies on ctapipe and matplotlib.
--- nsb/emitter/stars.py
+++ nsb/emitter/stars.py
@@ -14,37 +14,18 @@ from blacksky.catalog import StarCatalog, StarMap
 from nsb.core import Ray
 from nsb.core.emitter import Emitter, Diffuse
 
-class GaiaDR3(Emitter):
-    def compile(self):
-        catalog = np.load(self.config['catalog_file'])
-        mags = [catalog['phot_g_mean_mag'], catalog['phot_bp_mean_mag'], catalog['phot_rp_mean_mag']]
-        bpas = [bandpass.GaiaDR3_G(), bandpass.GaiaDR3_BP(), bandpass.GaiaDR3_RP()]
-        
-        catalog = StarCatalog.from_photometry(catalog['ra'], catalog['dec'], magnitudes=mags, bandpass=bpas, stis008=True)
-        v_mag = catalog.spectral.apply_bandpass(bandpass.OSN_V())
-        vmask = (v_mag > self.config['magmin']) & (v_mag <= self.config['magmax'])
-
-        self.catalog = catalog[vmask]
-        self.catalog.build_balltree()
-
+class GenericStarCatalog(Emitter):
     def emit(self, frame):
         target = frame.target.transform_to('icrs')
         wvl = frame.obswl.to(u.nm).value
-        ind, coords, spec = self.catalog.query(wvl, np.asarray([[target.dec.rad, target.ra.rad]]), np.deg2rad(frame.fov))
-
-        coords = SkyCoord(coords[:,1], coords[:,0], unit='rad', frame='icrs').transform_to(frame.AltAz)
         E_p = c.h.value*c.c.value / (wvl*1e-9)
+        
+        ind, coords, spec = self.catalog.query(wvl, np.asarray([[target.dec.rad, target.ra.rad]]), np.deg2rad(frame.fov))
+        coords = SkyCoord(coords[:,1], coords[:,0], unit='rad', frame='icrs').transform_to(frame.AltAz)
         
         return Ray(coords, weight=spec/E_p, source=type(self), parent=ind, direction='forward')
 
-class GaiaDR3Mag15(Diffuse):
-    def compile(self):
-        mag_map = np.load(self.config['magnitude_maps'])
-        mags = [mag_map[0], mag_map[1], mag_map[2]]
-        bpas = [bandpass.GaiaDR3_G(), bandpass.GaiaDR3_BP(), bandpass.GaiaDR3_RP()]
-        
-        self.catalog = StarMap.from_photometry_map(magnitudes=mags, bandpass=bpas, stis008=True)
-
+class GenericStarMap(Diffuse):
     def evaluate(self, frame, rays):
         r_g = rays.transform_to('icrs')
         wvl = frame.obswl.to(u.nm).value
@@ -54,6 +35,39 @@ class GaiaDR3Mag15(Diffuse):
         rays.source = type(self)
         
         return rays*(spec/E_p)
+
+class GaiaDR3(GenericStarCatalog):
+    def compile(self):
+        # Loading GaiaDR3 main catalog file
+        main_catalog = np.load(self.config['gaia_file'])
+        mags = [main_catalog['phot_g_mean_mag'], main_catalog['phot_bp_mean_mag'], main_catalog['phot_rp_mean_mag']]
+        bpas = [bandpass.GaiaDR3_G(), bandpass.GaiaDR3_BP(), bandpass.GaiaDR3_RP()]
+        
+        gaia = StarCatalog.from_photometry(main_catalog['ra'], main_catalog['dec'],
+                                           magnitudes=mags, bandpass=bpas, stis008=True)
+        # Loading GaiaDR3 Tycho supplementary catalog
+        supp_catalog = np.load(self.config['supp_file'])
+        mags = [supp_catalog['BTmag'], supp_catalog['VTmag'], supp_catalog['Vmag'], supp_catalog['Bmag'], supp_catalog['Hmag']]
+        bpas = [bandpass.Tycho_B(), bandpass.Tycho_V(), bandpass.OSN_V(), bandpass.OSN_B(), bandpass.Hipp_M()]
+        
+        supp = StarCatalog.from_photometry(supp_catalog['RA_ICRS_'], supp_catalog['DE_ICRS_'],
+                                           magnitudes=mags, bandpass=bpas, stis008=True)
+        # Combining catalogs
+        catalog = gaia+supp
+        # Filtering by visual magnitude
+        v_mag = catalog.spectral.apply_bandpass(bandpass.OSN_V())
+        vmask = (v_mag > self.config['magmin']) & (v_mag <= self.config['magmax'])
+
+        self.catalog = catalog[vmask]
+        self.catalog.build_balltree()
+
+class GaiaDR3Mag15(GenericStarMap):
+    def compile(self):
+        mag_map = np.load(self.config['magnitude_maps'])
+        mags = [mag_map[0], mag_map[1], mag_map[2]]
+        bpas = [bandpass.GaiaDR3_G(), bandpass.GaiaDR3_BP(), bandpass.GaiaDR3_RP()]
+        
+        self.catalog = StarMap.from_photometry_map(magnitudes=mags, bandpass=bpas, stis008=True)
 
 
 
